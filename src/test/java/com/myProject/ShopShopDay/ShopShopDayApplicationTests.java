2d290373586295d502a51910234de97225b3d178
Chan Tung Wai <tedcodingac@gmail.com>
2025-08-16 21:16:15 +0800

    update test config

--- src/test/java/com/myProject/ShopShopDay/ShopShopDayApplicationTests.java
+++ src/test/java/com/myProject/ShopShopDay/ShopShopDayApplicationTests.java
@@ -1,13 +1,19 @@
 package com.myProject.ShopShopDay;
 
+import com.myProject.ShopShopDay.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class ShopShopDayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
 	}
 
 }
