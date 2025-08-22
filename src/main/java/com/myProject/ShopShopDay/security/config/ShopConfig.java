530d68af671ec7e8923ac8242936e6c6c82aba28
Teddy Chan <152369490+code-teddy@users.noreply.github.com>
2025-08-22 10:20:33 +0800

    Update ShopConfig.java

--- src/main/java/com/myProject/ShopShopDay/security/config/ShopConfig.java
+++ src/main/java/com/myProject/ShopShopDay/security/config/ShopConfig.java
@@ -89,12 +89,14 @@ public class ShopConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:5100") // Allow this origin
+                        .allowedOrigins(
+                            "https://www.shopshopday.online/",
+                            "https://shopshopday.online/"
+                        ) // Allow this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials
             }
         };
     }
-    /*,"http://buynowdotcom.s3-website-ap-southeast-1.amazonaws.com"*/
 }
