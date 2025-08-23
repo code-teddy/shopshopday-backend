843ef2c3794272a3eb42a4f624de5c46ecfce42c
Chan Tung Wai <tedcodingac@gmail.com>
2025-08-23 09:09:33 +0800

    add test for deployment

--- src/test/java/com/myProject/ShopShopDay/ShopShopDayApplicationTests.java
+++ src/test/java/com/myProject/ShopShopDay/ShopShopDayApplicationTests.java
@@ -2,10 +2,8 @@ package com.myProject.ShopShopDay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ShopShopDayApplicationTests {
 
 	@Test
